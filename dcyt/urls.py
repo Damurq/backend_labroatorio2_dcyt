426fea6e4bfcb6bc05db67548b311652917b81be
Damurq <michaelmontero.idb@gmail.com>
2021-09-29 13:06:07 -0400

    change y test

--- dcyt/urls.py
+++ dcyt/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
@@ -8,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/",include('rest_framework.urls')),
     path("users/",include('users.urls')),
-    path("pensum/",include('pensum.urls'))
+    path("pensum/",include('pensum.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ test/1.js
@@ -0,0 +1,132 @@
+/* eslint-disable */
+const defaultObject = {
+    info: {
+        count: 0,
+        next: null,
+        pages: 0
+    },
+    results: []
+};
+
+
+/**
+ * 
+ * @param {*} url 
+ * @returns 
+ */
+async function request(url) {
+    try {
+        const response = await fetch(url)
+        if (!response.ok) {
+            return defaultObject
+        }
+        else {
+            const data = await response.text();
+            const json = JSON.parse(data);
+            return json;
+        }
+    }
+    catch (e) {
+        return defaultObject
+    }
+}
+/**
+ * 
+ * @param {*} labels 
+ * @param {*} data 
+ * @returns 
+ */
+function filterDataTable(labels, data) {
+    let newData = data.map((obj) => {
+        let objData = {}
+        labels.forEach((label) => {
+            objData[label] = obj[label];
+        })
+        return objData;
+    })
+    return newData
+}
+
+/**
+ * 
+ * @param {*} urlBase 
+ * @param {*} schema 
+ * @param {*} filters 
+ */
+async function filterD(urlBase = "", schema = "", filters = {}) {
+    let newUrl = urlBase + schema
+    Object.keys(filters).forEach((element, index) => {
+        if (index == 0) {
+            newUrl += "/?" + element + "=" + filters[element];
+        }
+        else {
+            newUrl += "&" + element + "=" + filters[element];
+        }
+    })
+    let data = await request(newUrl)
+    //console.log(newUrl)
+    return data
+}
+
+async function getAll(urlBase = "", schema = "", labels) {
+    let info = []
+    let next = typeof urlBase === "string" ? urlBase + schema : null
+    while (next !== null) {
+        let data = await request(next)
+        next = data.info.next
+        info.push(...filterDataTable(labels, data.results));
+    }
+    return info;
+}
+
+async function character() {
+    let data = await request("http://127.0.0.1:8000/users/list/")
+    console.log(data)
+    return data
+}
+
+async function episode() {
+    let data = await request("https://rickandmortyapi.com/api/episode")
+    return data.results
+}
+
+async function getDataBar(section, labels, label) {
+    let result = await getAll("https://rickandmortyapi.com/api/", section, labels)
+    let dta = result.map((d) => d[label])
+    return dta
+
+}
+
+character().then(function (data) {
+    console.log("bien")
+    console.log(data)
+    const $div = document.getElementById("root")
+    let source = () => {
+        let texto = ""
+        let s = data.map((src) => {
+            return src["photo"]
+        })
+        content = s.map((src) => {
+            return "<picture><img src='" + "http://127.0.0.1:8000" + src + "' alt=''></picture>"
+        })
+        content.forEach((e)=>{
+            texto +=e
+        } )
+        return texto
+    }
+    $div.innerHTML += source()
+}, function (reason) {
+    console.log("mal")
+    console.log(reason)
+});
+
+
+module.exports = {
+    getDataBar,
+    request,
+    filterD,
+    getAll,
+    character,
+    episode,
+    filterDataTable
+}
